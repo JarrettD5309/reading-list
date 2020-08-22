3308522034898e1e3707d90af3ed7a00e05e01d0
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-08-22 11:40:43 -0400

    testing

---
+++ client/build/precache-manifest.9556ce24ead104e354acfd0a02c907e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24dd4b2c6ae0c8418184",
+    "url": "/static/css/main.81675cde.chunk.css"
+  },
+  {
+    "revision": "24dd4b2c6ae0c8418184",
+    "url": "/static/js/main.22275b33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "2df38468741ff6b74317613514b2386e",
+    "url": "/index.html"
+  }
+];
